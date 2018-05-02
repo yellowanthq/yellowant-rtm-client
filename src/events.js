2ec8decfb65bbab4053a3c88f853531da2ebfe16
Manik Singh <manik@yellowant.com>
2018-05-02 21:14:13 +0530

    add yellowant rtm client

---
+++ src/events.js
@@ -0,0 +1,10 @@
+/** YellowAnt RTM inbound and outbound events */
+export default {
+  // OUTBOUND
+  YELLOWANT_MESSAGE: "yellowant_message", // YellowAnt message from client
+
+  // INBOUND
+  ERROR: "error", // client errors
+  YELLOWANT_COMMAND: "yellowant_command", // YellowAnt command from server
+  YELLOWANT_WEBHOOK_SUBSCRIPTION: "yellowant_webhook_subscription", // YellowAnt webhook subscription from server
+};
